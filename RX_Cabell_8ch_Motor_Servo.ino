0eda72df435b181e6281e548c558954e49cd5fc2
stanekTM <44894972+stanekTM@users.noreply.github.com>
2026-01-02 18:55:32 +0100

    Update comments and formatting in source files
    Improved header comments in RX_Cabell_8ch_Motor_Servo.ino for clarity and consistency. Added a blank line for readability in PWM_Frequency.h.

--- RX_Cabell_8ch_Motor_Servo.ino
+++ RX_Cabell_8ch_Motor_Servo.ino
@@ -1,13 +1,13 @@
 /*
-  ***************************************************************************************************************
-  Test RC receiver with "Cabell" protocol (motor-servo driver, telemetry) using OpenAVRc and OpenTX Multiprotocol
-  ***************************************************************************************************************
+  *******************************************************************************************************************
+  Test RC receiver with "Cabell" protocol (motor and servo driver, telemetry) using OpenAVRc and OpenTX Multiprotocol
+  *******************************************************************************************************************
   Simple RC receiver from my repository https://github.com/stanekTM/RX_Cabell_8ch_Motor_Servo
   
   Works with RC transmitters:
   OpenAVRc                   https://github.com/Ingwie/OpenAVRc_Dev
   Multiprotocol from my fork https://github.com/stanekTM/TX_FW_Multi_Stane
-  ***************************************************************************************************************
+  *******************************************************************************************************************
 */
 
 /*
